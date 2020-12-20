7fa5425003788aa350b7e8896fcad2854f43a23a
Antonio Bottelier <Antonio3338@gmail.com>
2020-12-20 15:23:31 +0100

    Initial Commit

---
+++ Scripts/Services/ServiceAttribute.cs
@@ -0,0 +1,8 @@
+namespace DN.Service
+{
+    /// <summary>
+    /// This attribute should be put above classes that need to be denoted as a service.
+    /// Only these classes can be created by the <see cref="ServiceLocator"/>
+    /// </summary>
+    public class ServiceAttribute : System.Attribute { }
+}
